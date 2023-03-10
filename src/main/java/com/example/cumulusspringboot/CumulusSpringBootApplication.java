94cc1e10b54a784dfd7366af713a445afd98e69d
amani-boussaa <amani.boussaa@esprit.tn>
2023-03-10 19:16:07 +0100

    Initial commit

---
+++ src/main/java/com/example/cumulusspringboot/CumulusSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.cumulusspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CumulusSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CumulusSpringBootApplication.class, args);
+    }
+
+}
