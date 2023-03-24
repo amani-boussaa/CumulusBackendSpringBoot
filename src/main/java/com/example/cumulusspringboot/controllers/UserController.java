6b808ed60cdb138b9ee59026efa1b5b65003eb7a
amani-boussaa <amani.boussaa@esprit.tn>
2023-03-24 02:26:17 +0100

    Crud user @CrossOrigin(origins = http://localhost:4200)

--- src/main/java/com/example/cumulusspringboot/controllers/UserController.java
+++ src/main/java/com/example/cumulusspringboot/controllers/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api2/v1/users")
 @AllArgsConstructor
