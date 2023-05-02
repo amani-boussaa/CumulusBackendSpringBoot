8b92f80a86a0980f446c5a1e0ae146b2af0d9c24
amani-boussaa <amani.boussaa@esprit.tn>
2023-05-02 17:58:27 +0100

    add user class

---
+++ src/main/java/com/example/cumulusspringboot/entities/User.java
@@ -0,0 +1,40 @@
+package com.example.cumulusspringboot.entities;
+
+import lombok.*;
+
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+    String username;
+    String email;
+    String password;
+    @Enumerated(EnumType.STRING)
+    Role role;
+    String verificationToken;
+    Boolean verified;
+    Date expiryDate;
+    String institution;
+    String description;
+    String address;
+    @Lob
+    private byte[] imagePath;
+
+//    private String providerUserId;
+//    private String provider;
+    private String phonenumber;
+
+}
