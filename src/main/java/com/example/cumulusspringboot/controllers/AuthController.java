c6b8b0d0536b01afd4f7f6540a4317f4f52733ce
amani-boussaa <amani.boussaa@esprit.tn>
2023-05-08 13:28:38 +0100

    dep

--- src/main/java/com/example/cumulusspringboot/controllers/AuthController.java
+++ src/main/java/com/example/cumulusspringboot/controllers/AuthController.java
@@ -16,7 +16,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
-@CrossOrigin(origins = "http://localhost:4200/")
+//@CrossOrigin(origins = "http://localhost:4200/")
+@CrossOrigin(origins = "*")
 
 @RestController
 @RequestMapping("/api/v1/auth")
